80f7bb6478c5b7b3ade475df2920ebe786b15528
Vy <vyom.j@protonmail.com>
2023-10-17 10:15:45 +0530

    chore: reformat

--- main.py
+++ main.py
@@ -58,54 +58,65 @@ wan_links = int(
     (input(f"Enter no. of WAN links (eg: {DEFAULT_WAN_LINKS}): ") or DEFAULT_WAN_LINKS)
 )
 
-ip, default_mask = network.split("/")
-default_mask = int(default_mask)
-ip = str_to_ipv4(ip)
-subnet_requirement = get_hosts(subnet_requirement)
 
-computed_subnet_size = [2 ** (math.floor(math.log2(i)) + 1) for i in subnet_requirement]
-print(f"\nSubnetting {network} for hosts - {computed_subnet_size}")
+def calculate_subnets(network, subnet_requirement, wan_links):
+    ip, default_mask = network.split("/")
+    default_mask = int(default_mask)
+    ip = str_to_ipv4(ip)
 
-new_ip = ip[::]
-for i, size in enumerate(computed_subnet_size):
-    new_cidr = 32 - round(math.log2(size))
-    network_addr = f"{ipv4_to_str(new_ip)}/{new_cidr}"
+    subnet_requirement = get_hosts(subnet_requirement)
+    computed_subnet_size = [
+        2 ** (math.floor(math.log2(i)) + 1) for i in subnet_requirement
+    ]
 
-    new_ip = shift_ipv4(new_ip, size)
+    print(f"\nSubnetting {network} for hosts - {computed_subnet_size}")
 
-    data.append(
-        [
-            network_addr,
-            cidr_to_subnet_mask(new_cidr),
-            f"{size - 2} hosts",
-            f"LAN{i + 1}",
-            ipv4_to_str(new_ip),
-        ]
+    new_ip = ip[::]
+    for i, size in enumerate(computed_subnet_size):
+        new_cidr = 32 - round(math.log2(size))
+        network_addr = f"{ipv4_to_str(new_ip)}/{new_cidr}"
+
+        new_ip = shift_ipv4(new_ip, size)
+
+        data.append(
+            [
+                network_addr,
+                cidr_to_subnet_mask(new_cidr),
+                f"{size - 2} hosts",
+                f"LAN{i + 1}",
+                ipv4_to_str(new_ip),
+            ]
+        )
+
+    for i in range(wan_links):
+        new_cidr = 32 - 2
+        new_ip = shift_ipv4(new_ip, 4)
+
+        data.append(
+            [
+                f"{ipv4_to_str(new_ip)}/{new_cidr}",
+                cidr_to_subnet_mask(new_cidr),
+                "2 hosts",
+                f"WAN link{i + 1}",
+                ipv4_to_str(new_ip),
+            ]
+        )
+    return data
+
+
+def print_table(data):
+    output = table2ascii(
+        header=[
+            "Network Address",
+            "Subnet Mask",
+            "Usable Hosts",
+            "Type",
+            "Broadcast Address",
+        ],
+        body=data,
+        style=PresetStyle.ascii_box,
     )
+    print(output)
 
-for i in range(wan_links):
-    new_cidr = 32 - 2
-    new_ip = shift_ipv4(new_ip, 4)
 
-    data.append(
-        [
-            f"{ipv4_to_str(new_ip)}/{new_cidr}",
-            cidr_to_subnet_mask(new_cidr),
-            "2 hosts",
-            f"WAN link{i + 1}",
-            ipv4_to_str(new_ip),
-        ]
-    )
-
-output = table2ascii(
-    header=[
-        "Network Address",
-        "Subnet Mask",
-        "Usable Hosts",
-        "Type",
-        "Broadcast Address",
-    ],
-    body=data,
-    style=PresetStyle.ascii_box,
-)
-print(output)
+print_table(calculate_subnets(network, subnet_requirement, wan_links))
